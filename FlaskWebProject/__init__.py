d0a4886d9e792de3a400d7db75b4219f7222b45c
unknown <pavithrakandi8@gmail.com>
2026-03-13 13:57:36 +0530

    Initial commit without secrets

---
+++ FlaskWebProject/__init__.py
@@ -0,0 +1,31 @@
+"""
+The flask application package.
+"""
+import logging
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_session import Session
+
+app = Flask(__name__)
+app.config.from_object(Config)
+# TODO: Add any logging levels and handlers with app.logger
+if not app.debug:
+    file_handler = logging.FileHandler('app.log')
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s'
+    )
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('CMS startup')
+    
+Session(app)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+import FlaskWebProject.views
